f37fa900d0bad22186984ffb380ec8347dd792ce
gadhadar <gadhadar@gmail.com>
2024-10-27 13:18:14 +0530

    Update timings to get a better match

--- opendbc/car/tests/test_fw_fingerprint.py
+++ opendbc/car/tests/test_fw_fingerprint.py
@@ -317,7 +317,7 @@ class TestFwFingerprintTiming:
                 'tesla': 0.1,
                 'toyota': 0.7,
                 'volkswagen': 0.65,
-                'byd': 0.5,
+                'byd': 0.3,
             },
             2: {
                 'ford': 1.6,
