60bd6d5fccd0c72b1a50a440e57feb4dad1742cb
gadhadar <gadhadar@gmail.com>
2024-10-27 12:12:55 +0530

    Trying to fix CRC

--- opendbc/car/byd/bydcan.py
+++ opendbc/car/byd/bydcan.py
@@ -1,10 +1,4 @@
-# from common.numpy_fast import clip, interp
 # from opendbc.car.can_definitions import CanData
-# from opendbc.car.byd.values import CAR
-
-# from cereal import car
-
-# SetDistance = car.CarState.CruiseState.SetDistance
 
 
 def compute_set_distance(state):
@@ -55,10 +49,9 @@ def create_can_steer_command(packer, steer_angle, steer_req, is_standstill, raw_
         "SET_ME_1_2": 1,
     }
 
-    dat = packer.make_can_msg("STEERING_MODULE_ADAS", 0, values)[2]
-    # crc = byd_checksum(0xaf, dat[:-1])
-    # values["CHECKSUM"] = crc
-    values["CHECKSUM"] = byd_checksum(0xaf, dat[:-1])
+    dat = packer.make_can_msg("STEERING_MODULE_ADAS", 0, values)[1]
+    crc = byd_checksum(0xaf, dat[:-1])
+    values["CHECKSUM"] = crc
     return packer.make_can_msg("STEERING_MODULE_ADAS", 0, values)
 
 
