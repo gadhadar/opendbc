efed83df5610e410e0e550410ba4bc572cd0f6e0
gadhadar <gadhadar@gmail.com>
2024-10-29 03:52:14 +0530

    Update test_fw_fingerprint.py

--- opendbc/car/tests/test_fw_fingerprint.py
+++ opendbc/car/tests/test_fw_fingerprint.py
@@ -307,7 +307,7 @@ class TestFwFingerprintTiming:
             1: {
                 'gm': 1.0,
                 'body': 0.1,
-                'byd': 0.2,
+                'byd': 0.1,
                 'chrysler': 0.3,
                 'ford': 1.5,
                 'honda': 0.45,
